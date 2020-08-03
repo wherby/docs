3669e6d356757fd9b22645a5900fd720a586efde
stevenz33 <steven.t.zhou@cn.pwc.com>
2020-08-03 14:00:02 +0800

    add db insert

---
+++ dbinsert/code/r1controller.scala
@@ -0,0 +1,16 @@
+def securityPriceFileRecord() = deadbolt.SubjectPresent()(parse.multipartFormData) {
+  implicit request => {
+    request.body.file("file").map {
+      file =>
+        val excelResult = ExcelToArray.excelToArray(file.ref.toFile.getAbsolutePath)
+        val priceSeqOpt = RecordToSecurityPrice.getInputExcel(excelResult)
+        priceSeqOpt match {
+          case Some(priceSeq) => Future(
+            securityPriceWrite.recordMultiple(priceSeq)
+          )
+            Future(Ok(s"${priceSeqOpt.get.length} records recorded."))
+          case None => Future(BadRequest("This does not seem to be a GSP output file."))
+        }
+    }.getOrElse(Future(BadRequest))
+  }
+}
